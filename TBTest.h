c3cb8c758f1be2b0e3058f72559918c78d50b8a8
tapanibarinoff <tapani.barinoff@mac.com>
2014-07-26 19:29:42 +0300

    added a test class

---
+++ TBTest.h
@@ -0,0 +1,13 @@
+//
+//  TBTest.h
+//  Git test
+//
+//  Created by Tapani Barinoff on 26.7.2014.
+//  Copyright (c) 2014 Tapani Barinoff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBTest : NSObject
+
+@end
---
+++ TBTest.m
@@ -0,0 +1,13 @@
+//
+//  TBTest.m
+//  Git test
+//
+//  Created by Tapani Barinoff on 26.7.2014.
+//  Copyright (c) 2014 Tapani Barinoff. All rights reserved.
+//
+
+#import "TBTest.h"
+
+@implementation TBTest
+
+@end
